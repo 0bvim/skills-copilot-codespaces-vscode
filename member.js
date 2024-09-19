683083c65680cf3aea774ba3182a287175d3c173
Vinicius Pereira <130008311+0bvim@users.noreply.github.com>
2024-09-19 03:00:25 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+  return {
+    name: 'John Doe',
+    skills: [
+      { skill: 'JavaScript', level: 'Expert' },
+      { skill: 'HTML', level: 'Intermediate' },
+      { skill: 'CSS', level: 'Intermediate' },
+    ],
+  };
+}
